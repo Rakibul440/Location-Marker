210e58248d9060a191103b7defcf2efc1a5b8797
Rakibul Islam <rakibulislammondal21@gmail.com>
2024-11-06 18:48:38 +0530

    Added project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+
+const {Server} = require('socket.io')
+
+const app = express()
+const port = 3000
+const server = http.createServer(app)
+const io = new Server(server)
+
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+
+io.on('connection',(socket)=>{
+
+    socket.on("send-location", function(data){
+        io.emit("receive-locaiton",{
+            id:socket.id,
+            ...data
+        })
+        console.log("user Connected : ",socket.id);  
+    })
+
+    socket.on('disconnet',function(){
+        io.emit('user-disconnect',socket.id)
+        console.log("dicon :",socket.id);
+        
+    })
+    
+})
+
+app.get('/',(req,res)=>{
+    
+    res.render('index.ejs')
+})
+
+
+
+server.listen(port,()=>{
+    console.log(`App is running on http://localhost:${port}/`)
+})
